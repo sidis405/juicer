b693549da30f4a9146e2e0cf432913ca63ddd5d7
Sidrit Trandafili <forge405@gmail.com>
2016-04-15 15:22:44 +0200

    It's a wrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,59 @@
 var elixir = require('laravel-elixir');
+var fs = require('fs');
+var del = require('del');
+var gulp = require('gulp');
+var task = elixir.Task;
+
+require('laravel-elixir-jade');
+require('elixir-juice');
+require('laravel-elixir-imagemin');
+
 elixir.config.assetsPath = 'src';
 elixir.config.publicPath = 'dist';
 elixir.config.viewPath = './';
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+elixir.extend('prioritiseJquery', function(message) {
+
+        new task('prioritiseJquery', function() {
+
+            var jquery = "src/assets/vendor/jquery";
+            
+            if (fs.lstatSync(jquery).isDirectory()) {
+
+                fs.rename(jquery, "src/assets/vendor/--jquery");
+
+            }
+
+    });
+
+});
 
 elixir(function(mix) {
-    mix.sass('app.scss');
-    mix.browserify('app.js');
+    mix.sass('app.scss').scripts('app.js').copy('bower_components/**/fonts', 'dist/fonts' );
+
+    mix.jade({
+        baseDir: './',
+        src: '/src/jade/',
+        dest: '/dist/',
+        search: ['*.jade'],
+        blade: false,
+        html: true,
+    });
+
+    del.sync('src/assets/vendor');
+
+    mix.bower({
+        src: 'bower_components/',
+        output: 'src/assets/vendor'
+    }).prioritiseJquery();
+
+    mix.scripts('../assets/vendor/**/*.js', 'dist/js/vendor.js').styles('../assets/vendor/**/*.css', 'dist/css/vendor.css').imagemin("/img", "dist/img/");
+
     mix.browserSync({
-        files: ['**/*.html', 'dist/js/**/*.js', 'dist/css/**/*.css'],
+        files: ['**/*.html', '**/*.jade', 'dist/js/**/*.js', 'dist/css/**/*.css'],
         proxy: undefined,
         server: {
-            baseDir: "./"
+            baseDir: "./dist"
         }
     });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,1 @@
-var Vue = require('vue');
-var crypto = require('crypto');
-
-var demo = new Vue({
-    el: '.crypto-app',
-    data: {
-        plaintext: 'I am mr robot',
-        hashes: ['sha512', 'sha384', 'sha256', 'sha224', 'sha1', 'md5'],
-        selected_hash: 'sha1'
-    },
-    computed: {
-        ciphertext: function() {
-            var hash = crypto.createHash(this.selected_hash);
-            hash.update(this.plaintext);
-            return hash.digest('hex');
-        }
-    }
-})
+console.log('app.js running');
